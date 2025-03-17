d5538b8a037b75509492e115948ece8ce373a3ac
X0fei <yukissora@icloud.com>
2025-03-17 16:00:12 +0300

    add/edit in develop

---
+++ Demo1703/AddEditProductWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Demo1703;
+
+public partial class AddEditProductWindow : Window
+{
+    public AddEditProductWindow()
+    {
+        InitializeComponent();
+    }
+}
--- Demo1703/MainWindow.axaml.cs
+++ Demo1703/MainWindow.axaml.cs
@@ -1,11 +1,15 @@
 using Avalonia.Controls;
+using Demo1703.Models;
+using System.Collections.Generic;
 
 namespace Demo1703;
 
 public partial class MainWindow : Window
 {
+    private List<Product> displayList = Utils.Context.products;
     public MainWindow()
     {
         InitializeComponent();
+        ProductListBox.ItemsSource = displayList;
     }
 }
