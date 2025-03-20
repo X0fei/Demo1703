9d704a223f9af3cf99e0ff52b98f5ba263c0a5cf
X0fei <yukissora@icloud.com>
2025-03-20 15:50:09 +0300

    partners and colors

--- Demo1703/Models/Product.cs
+++ Demo1703/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Demo1703.Models;
 
@@ -18,4 +19,24 @@ public partial class Product
     public virtual ICollection<PartnersProduct> PartnersProducts { get; set; } = new List<PartnersProduct>();
 
     public virtual ProductType TypeNavigation { get; set; } = null!;
+    public string PartnerNames
+    {
+        get
+        {
+            return string.Join(", ", PartnersProducts.Select(p => p.PartnerNavigation.Name));
+        }
+    }
+    public string ItemColor
+    {
+        get
+        {
+            DateOnly date = new DateOnly(2024, 01, 01);
+            switch(PartnersProducts.Where(p => p.SaleDate >= date).Select(p => p.ProductQuantity).Sum())
+            {
+                case < 10000: return "Salmon";
+                case < 60000: return "SandyBrown";
+                case >= 60000: return "PaleGreen";
+            }
+        }
+    }
 }
